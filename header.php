aa262dc528e13db99681679e73383511c3a5ffac
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-10-02 19:14:19 +0100

    update-menu-work-with-bootstrap-5

--- header.php
+++ header.php
@@ -13,10 +13,10 @@
       <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="localhost/wpsite2/">Home</a>
               <?php
             wp_nav_menu( array(
               'theme_location'  => 'header-menu',
